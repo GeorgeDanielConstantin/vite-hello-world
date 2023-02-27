db0c662384e8646ec700f47a08ab23aabcbbbc6c
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-02-27 21:38:05 +0100

    bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+  <div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+  background-color: red;
+  height: 100vh;
+  width: 1320px;
+  margin: auto;
+}
+</style>
